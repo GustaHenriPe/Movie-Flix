dccb879eb8883b5669a484db0b7f2d689abe6ed9
gustavo <guhhpj@gmail.com>
2025-03-11 22:46:04 -0300

    feat(dto): creates the category mapper and implements all dto logic in the controller, service and entity.

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -1,6 +1,9 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.controller.request.CategoryRequest;
+import br.com.movieflix.controller.response.CategoryResponse;
 import br.com.movieflix.entity.Category;
+import br.com.movieflix.mapper.CategoryMapper;
 import br.com.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/movieflix/category")
@@ -17,20 +21,25 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping()
-    public List<Category> getAllCategories(){
-        return categoryService.findAll();
+    public List<CategoryResponse> getAllCategories(){
+        List<Category> categories = categoryService.findAll();
+        return categories.stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
     }
 
     @PostMapping()
-    public Category saveCategory(@RequestBody Category category){
-        return categoryService.saveCategory(category);
+    public CategoryResponse saveCategory(@RequestBody CategoryRequest request){
+        Category newCategory = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.saveCategory(newCategory);
+        return CategoryMapper.toCategoryResponse(savedCategory);
     }
 
     @GetMapping("/{id}")
-    public Optional<Category> getCategoryById(@PathVariable Long id){
+    public CategoryResponse getCategoryById(@PathVariable Long id){
         Optional<Category> optionalCategory = categoryService.findById(id);
         if (optionalCategory.isPresent()){
-            return optionalCategory;
+            return CategoryMapper.toCategoryResponse(optionalCategory.get());
         }
         return null;
     }
