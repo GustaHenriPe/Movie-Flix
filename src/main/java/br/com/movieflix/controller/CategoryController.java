bfaa601014de15a9930e6aa6cd45e887be3a1fa0
gustavo <guhhpj@gmail.com>
2025-03-11 22:12:33 -0300

    feat(controller): adds save routes, list by id and delete by id.

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -4,21 +4,41 @@ import br.com.movieflix.entity.Category;
 import br.com.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieflix/category")
 @RequiredArgsConstructor
 public class CategoryController {
 
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @GetMapping()
-    public List<Category> getallCategories(){
+    public List<Category> getAllCategories(){
         return categoryService.findAll();
     }
+
+    @PostMapping()
+    public Category saveCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Category> getCategoryById(@PathVariable Long id){
+        Optional<Category> optionalCategory = categoryService.findById(id);
+        if (optionalCategory.isPresent()){
+            return optionalCategory;
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        categoryService.deleteById(id);
+    }
+
 }
+
